d72cb43ed78ef2ad3b5e7e13b1c6b188eced25dc
Samarth Keskar <samarth@uable.com>
2022-08-28 21:16:46 +0530

    generate colors and showing shaded completed. readme is also done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
+import React, { useState } from "react";
+import Values from "values.js";
 import "./App.css";
+import SingleColor from "./Components/SingleColor/SingleColor";
 
 function App() {
-  return <h3>Hello</h3>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#ffffff").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#ffffff"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App;
